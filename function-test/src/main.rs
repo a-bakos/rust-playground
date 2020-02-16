d52030356d68d070ec8b18ddd1408703674d9f50
a-bakos <bakosattila@fastem.com>
2020-02-16 15:04:34 +0000

    Function test

--- function-test/src/main.rs
+++ function-test/src/main.rs
@@ -1,11 +1,11 @@
-fn get_five() -> i8 {
-    5
+fn get_five(x: i8) -> i8 {
+    x // no semicolon here
 }
 
 fn main() {
     println!("Hello, function test!");
 
-    let is_it_five = get_five();
+    let is_it_five = get_five(5);
 
     println!("The value of the variable is {}", is_it_five);
 }
