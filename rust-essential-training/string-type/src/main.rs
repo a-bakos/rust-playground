7a2b5623052f956bb3d41186ff420e0a73647a9c
a-bakos <bakosattila@fastem.com>
2021-03-30 19:23:18 +0100

    Ownership

--- rust-essential-training/string-type/src/main.rs
+++ rust-essential-training/string-type/src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     println!("{}", message);
     message.push_str(" is home");
     println!("{}", message);
+
+    // String ownership
+    let rocket_fuel = String::from("RP-1");
+    // Shadow the prev variable
+    let rocket_fuel = process_fuel(rocket_fuel);
+    println!("Rocket_fuel is {}", rocket_fuel);
+
+    fn process_fuel(mut propellant: String) -> String {
+        println!("Processing propellant {}", propellant);
+        propellant.push_str(" V2");
+        // we're transferring ownership back
+        propellant
+    }
 }
