a9031d4c1d21355b8f03887a2730649f89157cd2
a-bakos <bakosattila@fastem.com>
2021-03-30 18:28:02 +0100

    Loops challenge

--- rust-essential-training/loops/src/main.rs
+++ rust-essential-training/loops/src/main.rs
@@ -78,4 +78,34 @@ fn main() {
         }
         println!();
     }
+
+    /**
+     * Loops challenge
+     */
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32;
+    let mut min: i32;
+    let mut mean: f32;
+
+    min = 0;
+    max = 0;
+    mean = 0.0;
+    for number in numbers.iter() {
+        if number > &max {
+            max = *number;
+        } else if number < &min {
+            min = *number;
+        }
+        mean += *number as f32;
+    }
+    mean = mean / numbers.len() as f32;
+
+    println!("Min: {}", min);
+    println!("Max: {}", max);
+    println!("Mean: {}", mean);
+
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Tests passed!");
 }
