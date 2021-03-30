52614ddcde14f5f049043660c11055d98dc3ecbb
a-bakos <bakosattila@fastem.com>
2021-03-30 19:47:43 +0100

    Borrowing

--- rust-essential-training/string-type/src/main.rs
+++ rust-essential-training/string-type/src/main.rs
@@ -10,10 +10,19 @@ fn main() {
     let rocket_fuel = process_fuel(rocket_fuel);
     println!("Rocket_fuel is {}", rocket_fuel);
 
+    let fuel_len = borrow_fuel(&rocket_fuel);
+    println!("Borrowed fuel length is {}", fuel_len);
+
     fn process_fuel(mut propellant: String) -> String {
         println!("Processing propellant {}", propellant);
         propellant.push_str(" V2");
         // we're transferring ownership back
         propellant
+    }
+
+    fn borrow_fuel(propellant: &String) -> usize {
+        println!("Borrowing propellant");
+        let length = propellant.len();
+        length
     }
 }
