7ec3c67fc56c9a4f89c982291255aea9d5c2742d
a-bakos <bakosattila@fastem.com>
2021-03-31 07:54:04 +0100

    Borrowing + references + slices

--- rust-essential-training/string-type/src/main.rs
+++ rust-essential-training/string-type/src/main.rs
@@ -20,9 +20,27 @@ fn main() {
         propellant
     }
 
+    // Borrowing
     fn borrow_fuel(propellant: &String) -> usize {
         println!("Borrowing propellant");
         let length = propellant.len();
         length
     }
+
+    // Mutable borrowing
+    let mut rocket_fuel_2 = String::from("RP-2");
+    fn mutable_borrow_fuel(propellant: &mut String) -> usize {
+        propellant.push_str(" + Added bit here");
+        let length = propellant.len();
+        length
+    }
+    mutable_borrow_fuel(&mut rocket_fuel_2);
+    println!("After processing rocket fuel 2: {}", rocket_fuel_2);
+
+    /**
+     * Slices
+     */
+    let new_message = String::from("Greetings from Earth!");
+    let earth_slice = &new_message[15..=15 + 5];
+    println!("The slice is {}", earth_slice);
 }
