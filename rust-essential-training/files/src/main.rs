63855f321b49f2915f1f1422f821e05bf7e84f7c
a-bakos <bakosattila@fastem.com>
2021-04-01 20:30:59 +0100

    Read file into string

--- rust-essential-training/files/src/main.rs
+++ rust-essential-training/files/src/main.rs
@@ -1,15 +1,20 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
-    if env::args().len() <= 2 {
-        println!("Program requires at least 2 args");
-        return;
-    }
+    // Number of args
+    // if env::args().len() <= 2 {
+    //     println!("Program requires at least 2 args");
+    //     return;
+    // }
 
     for (index, argument) in env::args().enumerate() {
         println!("Argument {} is {}", index, argument);
     }
 
-    let arg2 = env::args().nth(2).unwrap();
-    println!("arg2 is {}", arg2);
+    //let arg2 = env::args().nth(2).unwrap();
+    //println!("arg2 is {}", arg2);
+
+    // Read a file
+    let contents = fs::read_to_string("series.txt").unwrap();
+    println!("Content is {}", contents);
 }
