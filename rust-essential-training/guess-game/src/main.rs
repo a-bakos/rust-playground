8839db528fcf83d833ec0c52f70c2747c640a8b2
a-bakos <bakosattila@fastem.com>
2021-04-01 19:23:29 +0100

    Guess game challenge

---
+++ rust-essential-training/guess-game/src/main.rs
@@ -0,0 +1,29 @@
+use rand::prelude::*;
+use std::io;
+
+fn main() {
+    /**
+     * Challenge: higher or lower
+     */
+    let computers_number: i32 = thread_rng().gen_range(0..1000);
+    println!("Guess the number between 0 and 1000!");
+
+    loop {
+        let mut user_choice = String::new();
+        println!("Enter your guess:");
+        io::stdin().read_line(&mut user_choice);
+        let user_choice: i32 = user_choice.trim().parse().unwrap();
+
+        println!("Your choice: {}", user_choice);
+        println!("COMPUTER: {}", computers_number);
+
+        if user_choice > computers_number {
+            println!("Too big, try again.");
+        } else if user_choice < computers_number {
+            println!("Too small, try again.");
+        } else {
+            println!("That is correct, you win!");
+            break;
+        }
+    }
+}
