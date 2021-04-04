dc02c02f3ca09c64e075779e9289098b7b4f846f
a-bakos <bakosattila@fastem.com>
2021-04-04 19:26:55 +0100

    Structs

--- rust-essential-training/structs-tuples/src/main.rs
+++ rust-essential-training/structs-tuples/src/main.rs
@@ -1,5 +1,4 @@
-#[derive(Debug)]
-
+#[derive(Debug, Clone)]
 struct Shuttle {
     name: String,
     crew_size: u8,
@@ -12,8 +11,23 @@ fn main() {
         crew_size: 8,
         propellant: 83265.5,
     };
-    println!("Name is {}", vehicle.name);
+
+    let vehicle_2 = Shuttle {
+        name: String::from("Discovery"),
+        // this tells rust that any fields that are not explicitly
+        // set should be the same as the previous "vehicle"
+        // -- These two structs are separate instances. If we make changes
+        // to the first vehicle, after the second one has been instantiated
+        // using those values, those changes will not be reflected in the second
+        // vehicle.
+        ..vehicle
+    };
+
+    let vehicle_3 = Shuttle { ..vehicle.clone() };
 
     vehicle.name = String::from("Atlantis");
+    vehicle.crew_size = 6;
     println!("Vehicle is {:?}", vehicle);
+    println!("Vehicle 2 is {:?}", vehicle_2);
+    println!("Vehicle 3 is {:?}", vehicle_3);
 }
