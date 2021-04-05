aea21299ac8ef0b943d3cf55fd227bab9cbd9d7f
a-bakos <bakosattila@fastem.com>
2021-04-05 18:59:02 +0100

    Vectors

--- rust-essential-training/vectors-hashmaps/src/main.rs
+++ rust-essential-training/vectors-hashmaps/src/main.rs
@@ -3,6 +3,12 @@ fn main() {
     friends.push(String::from("Joey"));
     friends.push(String::from("Ross"));
     friends.push(String::from("Chandler"));
+    friends.push(String::from("Gunther"));
+
+    println!("Friends are: {:?}", friends);
+
+    let last = friends.pop();
+    println!("Last friend is {:?}", last);
 
     println!("Friends are: {:?}", friends);
 
