21d87e18180eff8b8418c5c7ecc5eba5d3027a8a
a-bakos <bakosattila@fastem.com>
2021-04-05 14:17:12 +0100

    Enum methods

--- rust-essential-training/enums-match/src/main.rs
+++ rust-essential-training/enums-match/src/main.rs
@@ -6,6 +6,16 @@ enum Shape {
     Triangle(f64, f64, f64), // side a b c
 }
 
+impl Shape {
+    fn get_perimeter(&self) -> f64 {
+        match *self {
+            Shape::Circle(r) => r * 2.0 * std::f64::consts::PI,
+            Shape::Rectangle(w, h) => (2.0 * w) + (2.0 * h),
+            Shape::Triangle(a, b, c) => a + b + c,
+        }
+    }
+}
+
 fn main() {
     let my_shape = Shape::Rectangle(1.2, 3.4);
     println!("My shape is {:?}", my_shape);
@@ -31,4 +41,7 @@ fn main() {
         }
     };
     println!("Result is {}", result);
+
+    let perimeter = my_shape.get_perimeter();
+    println!("Perimeter is {}", perimeter);
 }
