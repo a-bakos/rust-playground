383d320f7a523e0d3949164fa153b87b8d6e1329
a-bakos <bakosattila@fastem.com>
2021-04-05 18:25:12 +0100

    Errors

--- rust-essential-training/errors/src/main.rs
+++ rust-essential-training/errors/src/main.rs
@@ -1,7 +1,20 @@
+use std::fs;
+
 fn main() {
-    panic!("PROBLEM!");
+    //panic!("PROBLEM!");
     // Enable Backtrace:
     // Type this in the console
     // $Env:RUST_BACKTRACE=1
     // $Env:RUST_BACKTRACE=0 // Disable
+
+    // Recoverable errors:
+    // Missing file
+    let content = fs::read_to_string("some_file.txt");
+    println!("content is {:?}", content);
+
+    // File exists
+    // Unwrap is simple but will panic and fail if the file doesn't exist
+    //let content_exists = fs::read_to_string("hello.txt").unwrap();
+    let content_exists = fs::read_to_string("hello.txt");
+    println!("content from hello is {:?}", content_exists);
 }
