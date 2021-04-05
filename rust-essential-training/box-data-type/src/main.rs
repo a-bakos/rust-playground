d8d554b863dd156a19dcac127f71802bcb0c22c5
a-bakos <bakosattila@fastem.com>
2021-04-05 11:10:22 +0100

    Box challenge

--- rust-essential-training/box-data-type/src/main.rs
+++ rust-essential-training/box-data-type/src/main.rs
@@ -46,4 +46,24 @@ fn main() {
         "unboxed_vehicle size on stack is {} bytes.",
         mem::size_of_val(&unboxed_vehicle)
     );
+
+    /**
+     * Box Challenge
+     */
+    let one = Box::new(1);
+    let two = Box::new(2);
+    assert_eq!(*sum_boxes(one, two), 3);
+
+    let pi = Box::new(3.14159);
+    let e = Box::new(2.71828);
+    assert_eq!(*sum_boxes(pi, e), 5.85987);
+
+    println!("Tests passed");
+}
+
+// The function expects two Boxes as inputs,
+// extracts their values to add them together,
+// and packages the result into a new box to return.
+fn sum_boxes<T: std::ops::Add<Output = T>>(a: Box<T>, b: Box<T>) -> Box<T> {
+    Box::new(*a + *b)
 }
