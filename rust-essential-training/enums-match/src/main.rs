740af9cb42e95c933b1769205a108154f42b201f
a-bakos <bakosattila@fastem.com>
2021-04-05 14:04:24 +0100

    Enum

--- rust-essential-training/enums-match/src/main.rs
+++ rust-essential-training/enums-match/src/main.rs
@@ -1,12 +1,12 @@
 // Enum = enumeration
 #[derive(Debug)]
 enum Shape {
-    Circle,
-    Rectangle,
-    Triangle,
+    Circle(f64),
+    Rectangle(f64, f64),
+    Triangle(f64, f64, f64),
 }
 
 fn main() {
-    let my_shape = Shape::Rectangle;
+    let my_shape = Shape::Rectangle(1.2, 3.4);
     println!("My shape is {:?}", my_shape);
 }
