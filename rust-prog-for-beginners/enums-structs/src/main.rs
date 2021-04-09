8026d12b3175705f1df24f48d990b49656bde926
a-bakos <bakosattila@fastem.com>
2021-04-09 20:50:49 +0100

    Struct and enums v2

--- rust-prog-for-beginners/enums-structs/src/main.rs
+++ rust-prog-for-beginners/enums-structs/src/main.rs
@@ -9,20 +9,23 @@ struct Drink {
     volume: f64,
 }
 
+fn my_drink_flavour(drink: Drink) {
+    let my_drink_flavour = match drink.flavour {
+        DrinkFlavours::Strawberry => "Strawberry",
+        DrinkFlavours::Pineapple => "Pineapple",
+        DrinkFlavours::Coconut => "Coconut",
+    };
+    println!(
+        "My drink tastes {}-y and is {} ml.",
+        my_drink_flavour, drink.volume
+    );
+}
+
 fn main() {
     let my_drink = Drink {
         flavour: DrinkFlavours::Coconut,
         volume: 500.0,
     };
 
-    let my_drink_flavour = match my_drink.flavour {
-        DrinkFlavours::Strawberry => "Strawberry",
-        DrinkFlavours::Pineapple => "Pineapple",
-        DrinkFlavours::Coconut => "Coconut",
-    };
-
-    println!(
-        "My drink tastes {}-y and is {} ml.",
-        my_drink_flavour, my_drink.volume
-    );
+    my_drink_flavour(my_drink);
 }
