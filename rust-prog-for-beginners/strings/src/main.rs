86f75b562d6042d19a477258d2fac4cf5ed9da46
a-bakos <bakosattila@fastem.com>
2021-04-11 21:09:57 +0100

    String vs str

--- rust-prog-for-beginners/strings/src/main.rs
+++ rust-prog-for-beginners/strings/src/main.rs
@@ -23,4 +23,48 @@ fn main() {
         print_name(&item.name);
         println!("Count: {}", item.count);
     }
+
+    // challenge
+    let people = vec![
+        Person {
+            age: 7,
+            name: String::from("Jonah"),
+            color: String::from("Blue"),
+        },
+        Person {
+            age: 8,
+            name: String::from("Dinah"),
+            color: String::from("Green"),
+        },
+        Person {
+            age: 9,
+            name: String::from("Amy"),
+            color: String::from("Red"),
+        },
+        Person {
+            age: 22,
+            name: String::from("Glenn"),
+            color: String::from("White"),
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            person.name_and_color();
+        }
+    }
+}
+
+// Challenge
+
+struct Person {
+    age: i8,
+    name: String,
+    color: String,
+}
+
+impl Person {
+    fn name_and_color(&self) {
+        println!("Name is {}, and fav color is {}", self.name, self.color);
+    }
 }
