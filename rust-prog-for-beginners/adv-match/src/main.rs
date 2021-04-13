a25bf4f0aa1497d0cba2e640eff50e8d92f2e34d
a-bakos <bakosattila@fastem.com>
2021-04-13 07:54:54 +0100

    Advanced match

--- rust-prog-for-beginners/adv-match/src/main.rs
+++ rust-prog-for-beginners/adv-match/src/main.rs
@@ -1,4 +1,4 @@
-enum Tickets {
+enum Ticket {
     Backstage(f64, String),
     Vip(f64, String),
     Standard(f64),
@@ -6,18 +6,18 @@ enum Tickets {
 
 fn main() {
     let sold_tickets = vec![
-        Tickets::Backstage(250.0, String::from("Jon")),
-        Tickets::Vip(200.0, String::from("Mary")),
-        Tickets::Standard(100.0),
+        Ticket::Backstage(250.0, String::from("Jon")),
+        Ticket::Vip(200.0, String::from("Mary")),
+        Ticket::Standard(100.0),
     ];
 
     for ticket in sold_tickets {
         match ticket {
-            Tickets::Backstage(price, name) => {
+            Ticket::Backstage(price, name) => {
                 println!("Backstage Ticket is for: {}, cost: {}", name, price)
             }
-            Tickets::Vip(price, name) => println!("VIP Ticket is for: {}, cost: {}", name, price),
-            Tickets::Standard(price) => println!("Standard ticket, cost: {}", price),
+            Ticket::Vip(price, name) => println!("VIP Ticket is for: {}, cost: {}", name, price),
+            Ticket::Standard(price) => println!("Standard ticket, cost: {}", price),
         }
     }
 }
