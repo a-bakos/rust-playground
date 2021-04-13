b331d3111a8c065f4798dfb9a9ebdbcf292ede84
a-bakos <bakosattila@fastem.com>
2021-04-13 07:57:05 +0100

    Advanced match

--- rust-prog-for-beginners/adv-match/src/main.rs
+++ rust-prog-for-beginners/adv-match/src/main.rs
@@ -1,19 +1,20 @@
 enum Ticket {
-    Backstage(f64, String),
+    Backstage(f64, String, bool),
     Vip(f64, String),
     Standard(f64),
 }
 
 fn main() {
     let sold_tickets = vec![
-        Ticket::Backstage(250.0, String::from("Jon")),
+        Ticket::Backstage(250.0, String::from("Jon"), true),
         Ticket::Vip(200.0, String::from("Mary")),
         Ticket::Standard(100.0),
     ];
 
     for ticket in sold_tickets {
         match ticket {
-            Ticket::Backstage(price, name) => {
+            Ticket::Backstage(price, name, _) => {
+                // Ignore last parameter in the enum variant
                 println!("Backstage Ticket is for: {}, cost: {}", name, price)
             }
             Ticket::Vip(price, name) => println!("VIP Ticket is for: {}, cost: {}", name, price),
