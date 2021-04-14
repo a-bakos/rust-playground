516a0c53a8699d8481385d2ac57d530fe29f98e0
a-bakos <bakosattila@fastem.com>
2021-04-14 21:08:24 +0100

    Result data type

--- rust-prog-for-beginners/result-data-type/src/main.rs
+++ rust-prog-for-beginners/result-data-type/src/main.rs
@@ -18,11 +18,28 @@ fn print_choice(choice: &Menu) {
     println!("choice: {:?}", choice);
 }
 
+// If you want to return nothing from a function,
+// you can use the unit type: ()
+fn pick_choice(choice: &str) -> Result<(), String> {
+    // the ? mark will automatically perform a match operation:
+    // if the return value is Ok(), that'll be the variable's value,
+    // if it's Err(), it'll return from the wrapping function (Hint: String)
+    let choice: Menu = get_choice(choice)?;
+    // If the question mark above evaulates to Err(),
+    // the below code will never be executed:
+    print_choice(&choice);
+    Ok(()) // returning nothing, aka unit type in Ok()
+}
+
 fn main() {
+    // Method 1
     let choice: Result<Menu, _> = get_choice("main");
 
     match choice {
         Ok(choice) => print_choice(&choice),
         Err(e) => println!("error: {:?}", e),
     }
+
+    // Method 2
+    pick_choice("start");
 }
