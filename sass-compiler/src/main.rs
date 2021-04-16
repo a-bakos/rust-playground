19636a5151d75a9b40fa553f39788f4f51596d9f
a-bakos <bakosattila@fastem.com>
2021-04-16 08:04:07 +0100

    File io test

--- sass-compiler/src/main.rs
+++ sass-compiler/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let entry_file = fs::read_to_string("style.scss").unwrap();
+    println!("Content is {}", entry_file);
+
+    let mut css_file = String::new();
+    css_file.push_str(".row{background:red;}");
+
+    fs::write("style.css", css_file);
 }
