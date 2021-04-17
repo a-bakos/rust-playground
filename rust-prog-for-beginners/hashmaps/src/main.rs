95de2050e65922a91a9add8b463ea243ec2f11e9
a-bakos <bakosattila@fastem.com>
2021-04-17 19:30:41 +0100

    Hashmap

--- rust-prog-for-beginners/hashmaps/src/main.rs
+++ rust-prog-for-beginners/hashmaps/src/main.rs
@@ -1,3 +1,35 @@
+use std::collections::HashMap;
+
+// In hashmaps, the entries are not stored in order.
+// This differs from vectors.
+
+#[derive(Debug)]
+struct Contents {
+    content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut lockers = HashMap::new();
+    lockers.insert(
+        1,
+        Contents {
+            content: "stuff".to_owned(),
+        },
+    );
+    lockers.insert(
+        2,
+        Contents {
+            content: "keys".to_owned(),
+        },
+    );
+    lockers.insert(
+        3,
+        Contents {
+            content: "bag".to_owned(),
+        },
+    );
+
+    for (key, value) in lockers.iter() {
+        println!("{} {:?}", key, value);
+    }
 }
