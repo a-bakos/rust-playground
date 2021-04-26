bf66e5f2ace82dd0aeb3e98ef4cffa03aa864eaf
a-bakos <bakosattila@fastem.com>
2021-04-26 06:41:33 +0100

    Map combinator

---
+++ rust-prog-for-beginners/maps/src/main.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+struct User {
+    id: i32,
+    name: String,
+}
+
+fn find_user(name: &str) -> Option<i32> {
+    let name = name.to_lowercase();
+    match name.as_str() {
+        "sam" => Some(1),
+        "matt" => Some(5),
+        "dave" => Some(9),
+        _ => None,
+    }
+}
+
+fn main() {
+    let name: &str = "dave";
+    let user = find_user(name).map(|found| User {
+        id: found,
+        name: name.to_string(),
+    });
+    println!("{:?}", user);
+}
