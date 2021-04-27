baaa4c9f692427d5427ec4bbaa444b97bfb02696
a-bakos <bakosattila@fastem.com>
2021-04-27 07:10:35 +0100

    iterators

--- rust-prog-for-beginners/iterators/src/main.rs
+++ rust-prog-for-beginners/iterators/src/main.rs
@@ -9,5 +9,32 @@ fn main() {
 
     // better method #2
     let numbers2 = vec![1, 2, 3, 4, 5];
+    // _ means "some kind of a vector"
     let plus_one2: Vec<_> = numbers2.iter().map(|num| num + 1).collect();
+
+    // filter
+    let numbers3 = vec![1, 2, 3, 4, 5];
+    // _ means "some kind of a vector"
+    let new_nums: Vec<_> = numbers3.iter().filter(|num| num <= 3).collect();
+
+    // find
+    let numbers4 = vec![1, 2, 3, 4, 5];
+    // _ means "some kind of a vector"
+    let find_me: Option<i32> = numbers3.iter().find(|num| num == 3);
+
+    // count
+    let count = numbers.iter().count();
+
+    // last
+    // the reason last returns an option is
+    // because numbers can be an empty vector
+    let last: Option<i32> = numbers.iter().last();
+
+    // min / max
+    let min: Option<i32> = numbers.iter().min();
+    let max: Option<i32> = numbers.iter().max();
+
+    // take
+    // take the first X number of items
+    let take: Option<i32> = numbers.iter().take(3).collect();
 }
