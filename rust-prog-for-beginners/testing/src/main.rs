4baa0cbbfebfd67ed2f5a40eb85017ef5f44f228
a-bakos <bakosattila@fastem.com>
2021-05-01 09:04:47 +0100

    Doctests

--- rust-prog-for-beginners/testing/src/main.rs
+++ rust-prog-for-beginners/testing/src/main.rs
@@ -35,6 +35,8 @@ mod test {
     // a crate, and crate here refers to the top
     // of the (current) code (above)
     use crate::*;
+    // or
+    use super::*;
 
     #[test]
     fn check_all_caps() {
@@ -77,4 +79,56 @@ mod test {
         let expected = "ab".to_owned();
         assert_eq!(result, expected, "Should be ab");
     }
+}
+
+// FROM MICROSOFT COURSE
+// DOCTEST EXAMPLE
+/// This function divides two numbers.
+///
+/// # Example #1
+///
+/// ```
+/// let result = div2(10, 2);
+/// assert_eq!(result, 5);
+/// ```
+///
+/// # Example #2
+///
+/// ```
+/// let result = div2(6, 3);
+/// assert_eq!(result, 2);
+/// ```
+///
+/// # Panics
+///
+/// The function panics if the second argument is zero.
+///
+/// ```rust,should_panic
+/// // panics on division by zero
+/// playground::div2(10, 0);
+/// ```
+pub fn div2(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    }
+    a / b
+}
+
+/// This function subtracts two numbers.
+///
+/// # Example #1
+///
+/// ```
+/// let result = sub(9, 2);
+/// assert_eq!(result, 7);
+/// ```
+///
+/// # Example #2
+///
+/// ```
+/// let result = sub(6, 9);
+/// assert_eq!(result, -3);
+/// ```
+pub fn sub(a: i32, b: i32) -> i32 {
+    a - b
 }
