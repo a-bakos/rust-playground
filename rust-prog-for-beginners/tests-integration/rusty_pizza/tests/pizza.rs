795e4c7e826b2e34031c6e1b9ad9cb3e52de98b7
a-bakos <bakosattila@fastem.com>
2021-05-01 09:11:53 +0100

    Integration tests

---
+++ rust-prog-for-beginners/tests-integration/rusty_pizza/tests/pizza.rs
@@ -0,0 +1,17 @@
+use rusty_pizza::Pizza;
+
+#[test]
+fn can_make_pepperoni_pizza() {
+    let pizza = Pizza::pepperoni(12);
+    assert_eq!(pizza.topping, "pepperoni");
+    assert_eq!(pizza.inches, 12);
+}
+
+#[test]
+fn can_make_mozzarella_pizza() {
+    let pizza = Pizza::mozzarella(16);
+    assert_eq!(pizza.topping, "mozzarella");
+    assert_eq!(pizza.inches, 16);
+}
+
+//Now that our integration tests setup is done, we can execute the cargo test command to see the results
