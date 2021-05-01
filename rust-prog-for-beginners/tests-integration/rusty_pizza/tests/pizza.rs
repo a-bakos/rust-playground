3918a82232b75bad65d4862cac15409bc8f16dbc
a-bakos <bakosattila@fastem.com>
2021-05-01 09:13:52 +0100

    Integration tests

--- rust-prog-for-beginners/tests-integration/rusty_pizza/tests/pizza.rs
+++ rust-prog-for-beginners/tests-integration/rusty_pizza/tests/pizza.rs
@@ -14,4 +14,14 @@ fn can_make_mozzarella_pizza() {
     assert_eq!(pizza.inches, 16);
 }
 
-//Now that our integration tests setup is done, we can execute the cargo test command to see the results
+// Now that our integration tests setup is done, we can execute the cargo test
+// command to see the results
+// Inspecting the output reveals that Rust places each kind of test in a
+// different section: unit tests, integration tests, and documentation tests.
+// In the integration tests section, we can see that our two tests inside the
+// tests/pizzas.rs file were collected and executed by the test suite.
+// Only library crates can be tested via integration tests because binary crates
+// don't expose any functionality that other crates can use. But, many Rust
+// crates that provide a binary have a succinct src/main.rs file that calls
+// logic that lives in a src/lib.rs file. Using that structure, integration
+// tests can test the important parts of the code.
