e60c4927dba8bcfdfdf357da2ca58194dbe4c636
a-bakos <bakosattila@fastem.com>
2021-05-04 20:05:34 +0100

    p01

--- the-rust-programming-language/p01/src/main.rs
+++ the-rust-programming-language/p01/src/main.rs
@@ -2,6 +2,19 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+use std::mem;
+
 fn main() {
     println!("Hello, world!"); // println! is not a function, it's a macro
+
+    let x = 0; // unused variable aka dead code
+
+    let z: isize = 123;
+    let size_of_z: usize = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit OS",
+        z,
+        size_of_z,
+        size_of_z * 8
+    );
 }
