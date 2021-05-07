0e9eaed2a779340a0e210c09d2ddb019d38901bd
a-bakos <bakosattila@fastem.com>
2021-05-07 05:57:56 +0100

    Vectors

--- the-rust-programming-language/p03-standard-collections/src/main.rs
+++ the-rust-programming-language/p03-standard-collections/src/main.rs
@@ -7,4 +7,16 @@ fn main() {
     let index: usize = 0; // note, this is not an i32/64
     mynums[index] = 4;
     println!("{:?}", mynums[index]);
+
+    match mynums.get(5) {
+        Some(x) => println!("X is there = {:?}", x),
+        None => println!("No such element"),
+    }
+
+    mynums.push(100);
+    let last_elem = mynums.pop(); // Get last elem, remove it from Vec, and store it in last_elem as an Option
+    match last_elem {
+        Some(elem) => println!("This was the last element {:?}", elem),
+        None => println!("This was an empty vector..."),
+    }
 }
