de7ac43f0c1421ad6e546c68bb5e008ee243f5eb
a-bakos <bakosattila@fastem.com>
2021-05-09 12:52:08 +0100

    Traits

--- the-rust-programming-language/p05-traits/src/main.rs
+++ the-rust-programming-language/p05-traits/src/main.rs
@@ -13,8 +13,8 @@ struct Human {
 }
 
 impl Animal for Human {
-    fn new(name: &'static str) -> Self {
-        Self { name }
+    fn new(name: &'static str) -> Human {
+        Human { name }
     }
 
     fn name(&self) -> &'static str {
@@ -31,8 +31,8 @@ struct Cat {
 }
 
 impl Animal for Cat {
-    fn new(name: &'static str) -> Self {
-        Self { name }
+    fn new(name: &'static str) -> Cat {
+        Cat { name }
     }
 
     fn name(&self) -> &'static str {
@@ -45,15 +45,28 @@ impl Animal for Cat {
 }
 
 fn main() {
+    // Method 1
+
     let human = Human { name: "Frank" };
     human.talk();
 
     let cat = Cat { name: "Felix" };
     cat.talk();
 
+    // Method 2
+
     let human2 = Human::new("Grace");
     human2.talk();
 
     let cat2 = Cat::new("Molly");
     cat2.talk();
+
+    // Method 3 -- Using Animal to create, and the type of the variable
+    // helps the compiler decide if it's a Human or a Cat
+
+    let human3: Human = Animal::new("Kelly");
+    human3.talk();
+
+    let cat3: Cat = Animal::new("Doge");
+    cat3.talk();
 }
