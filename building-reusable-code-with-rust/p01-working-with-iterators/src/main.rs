946b2091b2cd75ba84d75643b43f0f02f16e97ed
a-bakos <bakosattila@fastem.com>
2021-05-20 06:07:25 +0100

    Working with iterators - Fold

--- building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
+++ building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
@@ -23,6 +23,17 @@ fn main() {
     let sum: i32 = numbers_3.iter().sum();
     println!("Sum: {}", sum);
 
+    // Max returns an option so need to unwrap
     let max: &i32 = numbers_3.iter().max().unwrap();
     println!("Max: {}", max);
+
+    // Fold to do aggregation on some complex logic
+    let numbers_4 = vec![1, 2, 3, 4];
+    let fold_sum: i32 = numbers_4.iter().fold(0, |sum, val| sum + val);
+    // fold:
+    // 	 param 1 = initial value
+    // 	 param 2 = a closure with two params:
+    //		- accumulator
+    //		- element
+    println!("Fold sum: {}", fold_sum);
 }
