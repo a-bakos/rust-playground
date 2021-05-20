9654156d808b2b898524c72f358bf580597bcbe5
a-bakos <bakosattila@fastem.com>
2021-05-20 06:01:21 +0100

    Working with iterators - Consuming adaptors

--- building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
+++ building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+    /**
+     * Generator adaptors
+     */
     // Map
     let numbers = vec![1, 2, 3, 4];
     let plus_one = numbers.iter().map(|x| x + 1);
@@ -10,4 +13,16 @@ fn main() {
     let numbers_2 = vec![1, 2, 3, 4];
     let larger_than_two = numbers_2.into_iter().filter(|&x| x > 2);
     larger_than_two.for_each(|x| println!("{}", x));
+
+    println!("");
+
+    /**
+     * Consuming adaptors
+     */
+    let numbers_3 = vec![1, 2, 3, 4];
+    let sum: i32 = numbers_3.iter().sum();
+    println!("Sum: {}", sum);
+
+    let max: &i32 = numbers_3.iter().max().unwrap();
+    println!("Max: {}", max);
 }
