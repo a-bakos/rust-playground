c4d7e33f65005f0732a2b38ad94428c390ee87fd
a-bakos <bakosattila@fastem.com>
2021-05-20 05:57:52 +0100

    Iterators

--- building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
+++ building-reusable-code-with-rust/p01-working-with-iterators/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
+    // Map
     let numbers = vec![1, 2, 3, 4];
-
     let plus_one = numbers.iter().map(|x| x + 1);
-
     plus_one.for_each(|x| println!("{}", x));
+
+    println!("");
+
+    // filter
+    let numbers_2 = vec![1, 2, 3, 4];
+    let larger_than_two = numbers_2.into_iter().filter(|&x| x > 2);
+    larger_than_two.for_each(|x| println!("{}", x));
 }
