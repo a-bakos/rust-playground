a7af70c4e9fc02c0e9ef3a9aa0c9aeff03bb6508
a-bakos <bakosattila@fastem.com>
2021-05-21 06:20:59 +0100

    Project init

---
+++ lott/src/main.rs
@@ -0,0 +1,16 @@
+// open csv
+// parse csv rows
+// sort draw numbers
+
+#[derive(Debug)]
+struct Draw {
+    id: String, // draw number
+    date: String,
+    balls: Vec<u8>,
+    set: u8,
+    machine: String,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
