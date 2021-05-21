9f24078d4aff0456359b50b40419d6780dc47bd9
a-bakos <bakosattila@fastem.com>
2021-05-21 06:31:12 +0100

    Project basics

--- lott/src/main.rs
+++ lott/src/main.rs
@@ -3,14 +3,37 @@
 // sort draw numbers
 
 #[derive(Debug)]
-struct Draw {
+pub struct Draw {
     id: String, // draw number
     date: String,
     balls: Vec<u8>,
     set: u8,
     machine: String,
 }
+impl Draw {
+    pub fn new(id: String, date: String, set: u8, machine: String) -> Draw {
+        Draw {
+            id,
+            date,
+            balls: vec![],
+            set,
+            machine,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DrawHistory {
+    all: Vec<Vec<u8>>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let draw_test = Draw::new(
+        "2651".to_string(),
+        "19-May-21".to_string(),
+        4,
+        "Lancelot".to_string(),
+    );
+
+    println!("{:?}", draw_test);
 }
