2bd35f9aa8aa2dc092ee5497218d57327c2f65f7
a-bakos <bakosattila@fastem.com>
2021-05-26 07:49:03 +0100

    Associated types WIP

---
+++ building-reusable-code-with-rust/p06-associated-types/src/main.rs
@@ -0,0 +1,12 @@
+/// Associated types
+/// vs Generics and Trait inheritance
+/*
+ * Associated types give you a way to specify
+ * a placeholder type in the function signature.
+ * So the implementor of a trait can specify
+ * the concrete type to use.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
