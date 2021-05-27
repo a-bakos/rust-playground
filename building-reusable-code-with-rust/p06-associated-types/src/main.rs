6cdc82d3f0942a5f7da9e86806b04ebc3dcabd6b
a-bakos <bakosattila@fastem.com>
2021-05-27 06:13:23 +0100

    Associated types

--- building-reusable-code-with-rust/p06-associated-types/src/main.rs
+++ building-reusable-code-with-rust/p06-associated-types/src/main.rs
@@ -6,7 +6,30 @@
  * So the implementor of a trait can specify
  * the concrete type to use.
  */
+use std::fmt;
+
+trait Iterator {
+    type Item: fmt::Display; // associated type and forced display trait - trait bounds
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+struct Counter;
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // implementation
+        Some(42)
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut counter = Counter {};
+
+    let n_int: Option<u32> = counter.next();
+    println!("{:?}", n_int);
+
+    let n_unknown = counter.next();
+    println!("{:?}", n_unknown);
 }
