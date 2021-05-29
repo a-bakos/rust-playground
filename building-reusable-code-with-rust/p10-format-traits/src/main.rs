fa80a3b977127b86b06d1855c799174d537c76df
a-bakos <bakosattila@fastem.com>
2021-05-29 14:41:22 +0100

    Default traits

--- building-reusable-code-with-rust/p10-format-traits/src/main.rs
+++ building-reusable-code-with-rust/p10-format-traits/src/main.rs
@@ -1,5 +1,8 @@
 use std::fmt::{Display, Formatter, Result};
 
+// If you have very complex data structures, you can consider implementing
+// Debug yourself
+
 #[derive(Debug)]
 struct Cat {
     name: &'static str,
@@ -32,4 +35,25 @@ fn main() {
 
     // Debug with pretty-print
     println!("{:#?}", a_cat);
+
+    // DEFAULT TRAIT
+    // use default values for all the fields
+    let data1: MyData = Default::default();
+    println!("{:?}", data1);
+
+    // specify part of the fields and use the defaults for the rest
+    let data_2 = MyData {
+        int_field: 42,
+        ..Default::default() // use the default for the rest
+    };
+    println!("{:?}", data_2);
+}
+
+// Default trait
+// Helpful when we want to define default values
+
+#[derive(Default, Debug)]
+struct MyData {
+    int_field: i32,
+    float_field: f32,
 }
