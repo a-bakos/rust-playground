5a9f2ac56f880af15c96fb03690dc72043acf9ab
a-bakos <bakosattila@fastem.com>
2021-06-10 05:35:03 +0100

    Simple result enum

---
+++ hands-on-data-structures-and-algorithms-in-rust/p01-results-options/src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+pub enum Res<T, E> {
+    Thing(T),
+    Error(E),
+}
+
+fn divide(a: i32, b: i32) -> Res<i32, String> {
+    if b == 0 {
+        return Res::Error("Cannot divide by zero".to_string());
+    }
+
+    Res::Thing(a / b)
+}
+
+fn main() {
+    let a = divide(10, 5);
+    let b = divide(1, 0);
+    println!("a = {:?}, b = {:?}", a, b);
+}
