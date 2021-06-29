5ceaf7e2585f655a77c7087a007522d924cd3865
a-bakos <bakosattila@fastem.com>
2021-06-29 06:53:48 +0100

    Bubble sorting

---
+++ hands-on-data-structures-and-algorithms-in-rust/p06-sorting/v1-sorting/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
+    for _ in 0..v.len() {
+        for i in 0..v.len() - 1 {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // inside a module you can use its parent
+    // module's properties using super
+    use super::*;
+    #[test]
+    fn test_bubble_sort() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        bubble_sort(&mut v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
+    }
+}
