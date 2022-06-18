60e86ba2ef0b23c15c261616301a10359f0fef01
a-bakos <bakosattila@fastem.com>
2022-06-18 18:36:00 +0100

    Cargo workspaces - Add another library

--- workspaces/add-one/src/lib.rs
+++ workspaces/add-one/src/lib.rs
@@ -6,9 +6,10 @@ pub fn add_one(x: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(3, add_one(2));
     }
 }
--- workspaces/adder/src/main.rs
+++ workspaces/adder/src/main.rs
@@ -1,12 +1,19 @@
 use add_one;
+use add_two;
 use rand;
 
 fn main() {
     let num = 10;
-
     println!(
         "Hello, world! {} plus one is {}",
         num,
         add_one::add_one(num)
+    );
+
+    let num = 10;
+    println!(
+        "Hello, world! {} plus one two {}",
+        num,
+        add_two::add_two(num)
     );
 }
