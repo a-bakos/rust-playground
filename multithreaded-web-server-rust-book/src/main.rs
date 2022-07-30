2aadd59a93ea86b2c2aa2aa450af352e3762f8f3
a-bakos <bakosattila@fastem.com>
2022-07-30 10:17:21 +0100

    Validate request

--- multithreaded-web-server-rust-book/src/main.rs
+++ multithreaded-web-server-rust-book/src/main.rs
@@ -8,11 +8,17 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let get = b"GET / HTTP/1.1\r\n";
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        // some other request
+    }
 
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
