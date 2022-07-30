873f5e1679a056d670f522aa681ea5fdb365bb57
a-bakos <bakosattila@fastem.com>
2022-07-30 10:25:19 +0100

    Refactor routing

--- multithreaded-web-server-rust-book/src/main.rs
+++ multithreaded-web-server-rust-book/src/main.rs
@@ -10,21 +10,17 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let response = format!("{}{}", status_line, contents);
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
