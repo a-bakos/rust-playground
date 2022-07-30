853a8492a02cd978a0bf70ff39abdd8e7f871402
a-bakos <bakosattila@fastem.com>
2022-07-30 11:56:59 +0100

    Implement Worker struct

--- multithreaded-web-server-rust-book/src/lib.rs
+++ multithreaded-web-server-rust-book/src/lib.rs
@@ -1,7 +1,20 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
+
+pub struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,13 +28,14 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for i in 0..size {
             // create some threads and store them in the vector
+            workers.push(Worker::new(i));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
